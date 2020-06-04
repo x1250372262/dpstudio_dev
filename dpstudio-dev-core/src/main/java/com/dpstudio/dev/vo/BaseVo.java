8ee84c026e4eda5b330025d06a1ce30217ef17a3
xujianpeng <1250372262@qq.com>
2020-06-04 15:00:32 +0800

    vo修改

--- dpstudio-dev-core/src/main/java/com/dpstudio/dev/vo/BaseVo.java
+++ dpstudio-dev-core/src/main/java/com/dpstudio/dev/vo/BaseVo.java
@@ -14,8 +14,26 @@ public abstract class BaseVo<T> implements Serializable {
 
     protected abstract T me();
 
-    public T copy(Object target) throws Exception {
-        return ClassUtils.wrapper(target).duplicate(me());
+    /**
+     * 从 目标类复制数据
+     *
+     * @param target
+     * @return
+     * @throws Exception
+     */
+    public void copy(Object target) throws Exception {
+        ClassUtils.wrapper(target).duplicate(me());
+    }
+
+    /**
+     * 把数据粘贴给目标类
+     *
+     * @param target
+     * @return
+     * @throws Exception
+     */
+    public void paste(Object target) throws Exception {
+        ClassUtils.wrapper(me()).duplicate(target);
     }
 
 }
