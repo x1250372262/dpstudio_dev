8990cc9a83c97396f9082e2ec75bf1e69b36e017
xujianpeng <1250372262@qq.com>
2020-08-10 17:45:27 +0800

    权限

--- dpstudio-dev-security/src/main/java/com/dpstudio/dev/security/taglib/PermissionTag.java
+++ dpstudio-dev-security/src/main/java/com/dpstudio/dev/security/taglib/PermissionTag.java
@@ -13,38 +13,23 @@ public class PermissionTag extends BodyTagSupport {
 
     private String permission;
 
-    private Boolean checkPermission(String permission) {
+    private Boolean checkPermission(String code) throws Exception {
 
-        IAuthenticator iAuthenticator = Security.get().getModuleCfg().getAuthenticator();
+        IAuthenticator iAuthenticator = com.dpstudio.dev.security.Security.get().getModuleCfg().getAuthenticator();
 
         if (iAuthenticator == null) {
             throw new NullArgumentException("authenticator_class");
         }
-
-        List<String> userPermissionCodes = null;
-        try {
-            //是总管理
-            if (iAuthenticator.isFounder()) {
-                return true;
-            }
-            userPermissionCodes = iAuthenticator.userPermission();
-        } catch (Exception e) {
-            e.printStackTrace();
+        //是总管理
+        if (iAuthenticator.isFounder()) {
+            return true;
         }
-
-        if (userPermissionCodes == null || userPermissionCodes.size() <= 0) {
+        List<String> userPermissionCodes = iAuthenticator.userPermission();
+        if (userPermissionCodes == null) {
             return false;
         }
-        if (StringUtils.isBlank(permission)) {
+        if (!userPermissionCodes.contains(code)) {
             return false;
-        }
-
-        String[] permissions = StringUtils.split(StringUtils.trimToEmpty(permission), "|");
-
-        for (String str : userPermissionCodes) {
-            if (!ArrayUtils.contains(permissions, str)) {
-                return false;
-            }
         }
         return true;
     }
@@ -53,11 +38,17 @@ public class PermissionTag extends BodyTagSupport {
     @Override
     public int doStartTag() {
         // 如果为true则显示文本中的内容，否则不显示
-        if (checkPermission(permission)) {
-            return EVAL_BODY_INCLUDE;
-        } else {
+        try {
+            if (checkPermission(permission)) {
+                return EVAL_BODY_INCLUDE;
+            } else {
+                return SKIP_BODY;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
             return SKIP_BODY;
         }
+
     }
 
     @Override
