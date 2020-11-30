3a3c17a9d634f8cbef27748a57a05a337a3b812e
xujianpeng <1250372262@qq.com>
2020-11-30 09:20:04 +0800

    L更新

--- dpstudio-dev-core/src/main/java/com/dpstudio/dev/core/L.java
+++ dpstudio-dev-core/src/main/java/com/dpstudio/dev/core/L.java
@@ -17,9 +17,12 @@ public class L<T> {
     /**
      * 查询列表
      */
-    public IView listView(IResultSet<T> resultSet, int page) {
+    public IView listView(IResultSet<T> resultSet, Integer page) {
+        assert resultSet != null;
         return WebResult.succeed().data(resultSet.getResultData())
                 .attr("total", resultSet.getRecordCount())
+                .attr("pageCount", resultSet.getPageCount())
+                .attr("pageNumber", resultSet.getPageNumber())
                 .attr("page", page).keepNullValue().toJsonView();
     }
 
@@ -27,16 +30,19 @@ public class L<T> {
      * 查询列表
      */
     public IView listView(IResultSet<T> resultSet) {
+        assert resultSet != null;
         return WebResult.succeed().data(resultSet.getResultData()).keepNullValue().toJsonView();
     }
 
     /**
      * 详情
+     *
      * @param t
      * @return
      * @throws Exception
      */
-    public IView detailView(T t) throws Exception{
+    public IView detailView(T t) {
+        assert t != null;
         return WebResult.succeed().data(t).keepNullValue().toJsonView();
     }
 }
