a2c4009a0a182ba15e91aab0f94d2690b9bd397b
xujianpeng <1250372262@qq.com>
2021-01-04 21:44:31 +0800

    修复单元测试不支持注入

--- dpstudio-dev-core/src/main/java/com/dpstudio/dev/support/junit/YmpJUnit4ClassRunner.java
+++ dpstudio-dev-core/src/main/java/com/dpstudio/dev/support/junit/YmpJUnit4ClassRunner.java
@@ -1,16 +1,27 @@
 package com.dpstudio.dev.support.junit;
 
+import net.ymate.platform.core.IApplication;
+import net.ymate.platform.core.IApplicationInitializer;
 import net.ymate.platform.core.YMP;
+import net.ymate.platform.core.beans.BeanMeta;
+import net.ymate.platform.core.beans.IBeanFactory;
+import net.ymate.platform.core.beans.IBeanLoader;
+import net.ymate.platform.core.event.Events;
+import net.ymate.platform.core.module.ModuleManager;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
 
 
 /**
- * @Author: 徐建鹏.
+ * @Author: mengxinag.
  * @create: 2021-01-04 17:58
  * @Description:
  */
 public class YmpJUnit4ClassRunner extends BlockJUnit4ClassRunner {
+
+    private final Class<?> targetClass;
+
+    private Object target;
 
     /**
      * Creates a BlockJUnit4ClassRunner to run {@code klass}
@@ -20,11 +31,42 @@ public class YmpJUnit4ClassRunner extends BlockJUnit4ClassRunner {
      */
     public YmpJUnit4ClassRunner(Class<?> klass) throws InitializationError {
         super(klass);
+        this.targetClass = klass;
+
         try {
-            YMP.run();
+            YMP.run(new YmpJunitApplicationInitializer());
+            this.target = YMP.get().getBeanFactory().getBean(targetClass);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        YMP.get().getBeanFactory().registerBean(klass);
     }
+
+    @Override
+    public Object createTest() throws Exception {
+        return this.target;
+    }
+
+    private class YmpJunitApplicationInitializer implements IApplicationInitializer {
+        @Override
+        public void afterEventInit(IApplication application, Events events) {
+
+        }
+
+        @Override
+        public void beforeBeanLoad(IApplication application, IBeanLoader beanLoader) {
+
+        }
+
+        @Override
+        public void beforeModuleManagerInit(IApplication application, ModuleManager moduleManager) {
+
+        }
+
+        @Override
+        public void beforeBeanFactoryInit(IApplication application, IBeanFactory beanFactory) {
+            BeanMeta beanMeta = BeanMeta.create(targetClass, true);
+            beanFactory.registerBean(beanMeta);
+        }
+    }
+
 }
