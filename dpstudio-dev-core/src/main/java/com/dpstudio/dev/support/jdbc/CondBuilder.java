f50e59d1ca5a4d749f4f824721566924e70c53b2
xujianpeng <1250372262@qq.com>
2021-02-01 11:35:32 +0800

    新增jdbc cond查询帮助类

---
+++ dpstudio-dev-core/src/main/java/com/dpstudio/dev/support/jdbc/CondBuilder.java
@@ -0,0 +1,101 @@
+package com.dpstudio.dev.support.jdbc;
+
+import cn.hutool.core.util.StrUtil;
+import com.dpstudio.dev.support.jdbc.annotation.CondConf;
+import net.ymate.platform.persistence.jdbc.query.Cond;
+
+import java.lang.reflect.Field;
+
+/**
+ * @Author: mengxiang.
+ * @create: 2021-02-01 09:26
+ * @Description:
+ */
+public class CondBuilder {
+
+    private static Cond condTarget;
+
+    private CondBuilder(boolean eqOne) {
+        if (eqOne) {
+            condTarget = Cond.create().eqOne();
+        } else {
+            condTarget = Cond.create();
+        }
+    }
+
+    public static CondBuilder create(boolean eqOne) {
+        return new CondBuilder(eqOne);
+    }
+
+
+    public Cond build(Object condBean) throws Exception {
+        Class<?> clazz = condBean.getClass();
+        if (clazz.isAnnotationPresent(com.dpstudio.dev.support.jdbc.annotation.CondBuilder.class)) {
+            Field[] fields = clazz.getDeclaredFields();
+            if (fields.length <= 0) {
+                return condTarget;
+            }
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(CondConf.class)) {
+                    field.setAccessible(true);
+                    Object value = field.get(condBean);
+                    CondConf condConf = field.getAnnotation(CondConf.class);
+                    switch (condConf.opt()) {
+                        case EQ:
+                            if (condConf.exprNotEmpty()) {
+                                condTarget.exprNotEmpty(value, cond -> cond.and().eqWrap(condConf.prefix(), condConf.field()).param(value));
+                            } else {
+                                condTarget.and().eqWrap(condConf.prefix(), condConf.field()).param(value);
+                            }
+                            break;
+                        case NOT_EQ:
+                            if (condConf.exprNotEmpty()) {
+                                condTarget.exprNotEmpty(value, cond -> cond.and().notEqWrap(condConf.prefix(), condConf.field()).param(value));
+                            } else {
+                                condTarget.and().notEqWrap(condConf.prefix(), condConf.field()).param(value);
+                            }
+                            break;
+                        case LT:
+                            if (condConf.exprNotEmpty()) {
+                                condTarget.exprNotEmpty(value, cond -> cond.and().ltWrap(condConf.prefix(), condConf.field()).param(value));
+                            } else {
+                                condTarget.and().ltWrap(condConf.prefix(), condConf.field()).param(value);
+                            }
+                            break;
+                        case GT:
+                            if (condConf.exprNotEmpty()) {
+                                condTarget.exprNotEmpty(value, cond -> cond.and().gtWrap(condConf.prefix(), condConf.field()).param(value));
+                            } else {
+                                condTarget.and().gtWrap(condConf.prefix(), condConf.field()).param(value);
+                            }
+                            break;
+                        case LT_EQ:
+                            if (condConf.exprNotEmpty()) {
+                                condTarget.exprNotEmpty(value, cond -> cond.and().ltEqWrap(condConf.prefix(), condConf.field()).param(value));
+                            } else {
+                                condTarget.and().ltEqWrap(condConf.prefix(), condConf.field()).param(value);
+                            }
+                            break;
+                        case GT_EQ:
+                            if (condConf.exprNotEmpty()) {
+                                condTarget.exprNotEmpty(value, cond -> cond.and().gtEqWrap(condConf.prefix(), condConf.field()).param(value));
+                            } else {
+                                condTarget.and().gtEqWrap(condConf.prefix(), condConf.field()).param(value);
+                            }
+                            break;
+                        case LIKE:
+                            if (condConf.exprNotEmpty()) {
+                                condTarget.exprNotEmpty(value, cond -> cond.and().likeWrap(condConf.prefix(), condConf.field()).param(StrUtil.format(condConf.likeExpression(), value)));
+                            } else {
+                                condTarget.and().likeWrap(condConf.prefix(), condConf.field()).param(StrUtil.format(condConf.likeExpression(), value));
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+        return condTarget;
+    }
+}
---
+++ dpstudio-dev-core/src/main/java/com/dpstudio/dev/support/jdbc/annotation/CondBuilder.java
@@ -0,0 +1,9 @@
+package com.dpstudio.dev.support.jdbc.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface CondBuilder {
+}
