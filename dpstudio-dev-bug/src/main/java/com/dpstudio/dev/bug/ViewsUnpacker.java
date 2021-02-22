01c3f85e6266f3646fb06cc0fe42947e9b4623d2
xujianpeng <1250372262@qq.com>
2021-02-22 15:58:28 +0800

    移除bug模块

--- dpstudio-dev-bug/src/main/java/com/dpstudio/dev/bug/ViewsUnpacker.java
+++
@@ -1,9 +0,0 @@
-package com.dpstudio.dev.bug;
-
-
-import net.ymate.module.unpack.IUnpack;
-import net.ymate.module.unpack.annotation.Unpack;
-
-@Unpack("dpstudio.bug")
-public class ViewsUnpacker implements IUnpack {
-}
